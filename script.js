88b83dc33afa319e856f267f6404ffec9658ff61
Ryan Oddi <ryanoddi@gmail.com>
2021-01-26 16:02:17 -0500

    add slider to html, add styling and display value of slider position on screen with JS

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// variable for current mouse position
+let currentlyActive = false;
+let currentColor = [];
+let cell = [];
+
 // create variable to store container
 const container = document.getElementById("container");
+
 // create a div and make a square
 function makeRows(rows, cols) {
   container.style.setProperty("--grid-rows", rows);
@@ -9,6 +15,13 @@ function makeRows(rows, cols) {
     container.appendChild(cell).className = "grid-item";
   }
 }
-// hover effect when mousing over squares
-
 makeRows(16, 16);
+// Slider
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
